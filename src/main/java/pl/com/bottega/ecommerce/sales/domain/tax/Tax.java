afa9dbcbced839d83f1ec0b452f219f3ab79101b
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-03-20 23:13:29 +0100

    Usunieto wczesniejszy sposob liczenia podatku

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/tax/Tax.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/tax/Tax.java
@@ -38,31 +38,4 @@ public class Tax {
 	public String getDescription() {
 		return description;
 	}
-	
-	public static Tax evaluateTax(RequestItem item) {
-		Money net = item.getTotalCost();
-		BigDecimal ratio = null;
-		String desc = null;
-		
-		switch (item.getProductData().getType()) {
-		case DRUG:
-			ratio = BigDecimal.valueOf(0.05);
-			desc = "5% (D)";
-			break;
-		case FOOD:
-			ratio = BigDecimal.valueOf(0.07);
-			desc = "7% (F)";
-			break;
-		case STANDARD:
-			ratio = BigDecimal.valueOf(0.23);
-			desc = "23%";
-			break;
-		default:
-			throw new IllegalArgumentException(item.getProductData().getType() + " not handled");	
-		}					
-		Money taxValue = net.multiplyBy(ratio);
-		
-		return new Tax(taxValue, desc);
-	}
-
 }
